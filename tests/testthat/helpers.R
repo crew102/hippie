14930ed2a07f6a4756093b226413d1f5f42186f3
chris <chriscrewbaker@gmail.com>
2022-03-11 23:02:56 -0500

    Refactor tests so both modes can be tested

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,7 +1,11 @@
-expect_hippie_match <- function(cursor_posish, expected_match, code = NULL,
-                                envir = parent.frame()) {
+expect_hippie_matches_across_modes <- function(cursor_posish,
+                                               expected_match,
+                                               sequence,
+                                               mode,
+                                               envir = parent.frame()) {
+
   skip_if_not(rstudioapi::isAvailable())
-  withr::local_options(list("hippie.mode" = "select"))
+  withr::local_options(list("hippie.mode" = mode))
   fixture_file <- test_path("src-fixture.R")
   tmp_file <- fs::file_temp()
 
@@ -20,11 +24,32 @@ expect_hippie_match <- function(cursor_posish, expected_match, code = NULL,
   tmp_file_id <- tmp_content$id
   rstudioapi::setCursorPosition(cursor_posish, tmp_file_id)
 
-  code
+  for (direction in sequence) {
+    if (direction == "down") hippie_down() else hippie_up()
+    Sys.sleep(.2)
+  }
 
   src_post_hippie <- rstudioapi::getSourceEditorContext()
-  matched_token <- src_post_hippie$selection[[1]]$text
+
+  if (mode == "select") {
+    matched_token <- src_post_hippie$selection[[1]]$text
+  } else {
+    matched_token <- extract_nearest_token(src_post_hippie)
+  }
 
   expect_identical(matched_token, expected_match)
   withr::deferred_run(envir = envir)
+}
+
+expect_hippie_match <- function(cursor_posish, expected_match,
+                                sequence, modes = c("select", "insert")) {
+  for (mode in modes) {
+    expect_hippie_matches_across_modes(
+      cursor_posish = cursor_posish,
+      expected_match = expected_match,
+      sequence = sequence,
+      mode = mode,
+      envir = parent.frame()
+    )
+  }
 }
--- tests/testthat/test-interactive.R
+++ tests/testthat/test-interactive.R
@@ -1,72 +1,54 @@
 # These tests have to be run interactively in RStudio by sourcing this file.
 
-context("Interactive tests")
-
 test_that("Target chars within string literal work", {
-  expect_hippie_match(c(5, 11), "matching_token", code = hippie_up())
-  expect_hippie_match(c(5, 11), "matching_token", code = hippie_down())
+  expect_hippie_match(c(5, 11), "matching_token", sequence = "up")
+  expect_hippie_match(c(5, 11), "matching_token", sequence = "up")
 })
 
 test_that("Target chars within assigned string literal work", {
-  expect_hippie_match(c(7, 18), "matching_", code = hippie_up())
-  expect_hippie_match(c(7, 18), "matc", code = {
-    hippie_up()
-    hippie_up()
-  })
-  expect_hippie_match(c(7, 18), "match", code = hippie_down())
+  expect_hippie_match(c(7, 18), "matching_", sequence = "up")
+  expect_hippie_match(c(7, 18), "matc", sequence = c("up", "up"))
+  expect_hippie_match(c(7, 18), "match", sequence = "down")
 })
 
 test_that("Candidate matches are recycled/iterated through correctly", {
-  expect_hippie_match(c(7, 18), "match", code = {
-    hippie_up()
-    hippie_up()
-    hippie_up()
-    hippie_up()
-    hippie_up()
-  })
-  expect_hippie_match(c(7, 18), "matching", code = {
-    hippie_up()
-    hippie_down()
-    hippie_down()
-  })
+  expect_hippie_match(c(7, 18), "match", sequence = rep("up", 5))
+  expect_hippie_match(c(7, 18), "matching", sequence = c("up", "down", "down"))
 })
 
 test_that("Target chars within substring work", {
-  expect_hippie_match(c(9, 16), "matching_", code = hippie_up())
+  expect_hippie_match(c(9, 16), "matching_", sequence = "up")
 })
 
 test_that("Numeric target works", {
-  expect_hippie_match(c(13, 2), "19847", code = hippie_up())
+  expect_hippie_match(c(13, 2), "19847", sequence = "up")
 })
 
 test_that("Targets within strings with escape chars works", {
-  expect_hippie_match(c(15, 33), "matching_", code = {
-    hippie_up()
-    hippie_up()
-  })
+  expect_hippie_match(c(15, 33), "matching_", sequence = c("up", "up"))
 })
 
 test_that("No matching candidate matches means no text is selected", {
-  expect_hippie_match(c(17, 20), "", code = hippie_up())
+  expect_hippie_match(c(17, 20), "", sequence = "up", modes = "select")
 })
 
 test_that("No target token means no text is selected", {
-  expect_hippie_match(c(26, 3), "", code = hippie_up())
+  expect_hippie_match(c(26, 3), "", sequence = "up")
 })
 
 test_that("Candidate matches extend to tokens with . in them", {
-  expect_hippie_match(c(20, 5), "blah.blah", code = hippie_up())
+  expect_hippie_match(c(20, 5), "blah.blah", sequence = "up")
 })
 
 test_that("Target chars inside comments work", {
-  expect_hippie_match(c(22, 17), "matching_", code = hippie_up())
+  expect_hippie_match(c(22, 17), "matching_", sequence = "up")
 })
 
 test_that("Intra-line matching works", {
-  expect_hippie_match(c(24, 19), "funny_object", code = hippie_up())
-  expect_hippie_match(c(25, 3), "funny", code = hippie_down())
+  expect_hippie_match(c(24, 19), "funny_object", sequence = "up")
+  expect_hippie_match(c(25, 3), "funny", sequence = "down")
 })
 
 test_that("Non-parsable lines are not a problem", {
-  expect_hippie_match(c(27, 22), "matching", code = hippie_up())
+  expect_hippie_match(c(27, 22), "matching", sequence = "up")
 })
